9e6720b37506b69921ceaa1f6f729c420760617e
charafeddine-Web <charafeddinetbibzat@gmail.com>
2025-02-25 18:57:21 +0000

    fix buge

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -146,6 +146,7 @@
                     <div class="shrink-0 me-3">
                         <img class="h-10 w-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
                     </div>
+
                 @endif
 
                 <div>
